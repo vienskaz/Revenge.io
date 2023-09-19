86a5fe1e42655bd9eababc265b986ca1d1ddc051
Adam <adwiesek@wp.pl>
2023-09-19 19:32:23 +0200

    premade item-detail

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -4,4 +4,15 @@ from .models import Item
 class ItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
+    def formatted_description(self, obj):
+        return f'<pre>{obj.description}</pre>'
+
+    formatted_description.allow_tags = True
+    formatted_description.short_description = 'Description'
+
+    def formatted_care_instructions(self, obj):
+        return f'<pre>{obj.care_instructions}</pre>'
+
+    formatted_care_instructions.allow_tags = True
+    formatted_care_instructions.short_description = 'Care Instructions'
 admin.site.register(Item,ItemAdmin)
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 class Item(models.Model):
   name = models.CharField(max_length=50)
-  description = models.CharField(max_length=300)
-  price = models.FloatField()
+  description = models.TextField(max_length=500)
+  price = models.DecimalField(max_digits=8, decimal_places=2) 
   image=models.ImageField(upload_to="images", null=True,default="default.jpg")
+  care_instructions=models.TextField(max_length=500)
   slug=models.SlugField(unique=True, db_index=True)
 
   def __str__(self):
     return f'{self.name} {self.price}' 
+  
